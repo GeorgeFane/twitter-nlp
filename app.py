4b8a97387de9f0fb22331d41b660fcd5fd3c94be
GeorgeFane <georgefane11@gmail.com>
2021-02-28 19:59:34 +0000

    web form and learning bayes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import *
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'hello'
+    return render_template(
+        'index.html',
+    )
 
 app.run(debug=True, port=8080)
